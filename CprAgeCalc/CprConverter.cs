06452007cb450b6d6bec0626fce699cd739d339d
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-09-18 11:38:37 +0200

    Added previous pair programming stuff

---
+++ CprAgeCalc/CprConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PairProgramming
+{
+    public static class CprConverter
+    {
+        public static int GetYear(int cpr)
+        {
+            var lastFour = cpr % 10000;
+            var lastTwoOfYear = (cpr / 10000) % 100;
+
+            int firstTwo;
+
+            if(lastFour <= 3999)
+            {
+                firstTwo = 19;
+            }else if (lastTwoOfYear <= 36 && lastFour <= 4999)
+            {
+                firstTwo = 20;
+            }
+            else if (lastFour <= 4999)
+            {
+                firstTwo = 19;
+            }
+            else if (lastTwoOfYear <= 57 && lastFour <= 5999)
+            {
+                firstTwo = 20;
+            }
+            else if (lastFour <= 5999)
+            {
+                firstTwo = 18;
+            }
+            else if (lastTwoOfYear <= 57 && lastFour <= 6999)
+            {
+                firstTwo = 20;
+            }
+            else if (lastFour <= 6999)
+            {
+                firstTwo = 18;
+            }
+            else if (lastTwoOfYear <= 57 && lastFour <= 7999)
+            {
+                firstTwo = 20;
+            }
+            else if (lastFour <= 7999)
+            {
+                firstTwo = 18;
+            }
+            else if (lastTwoOfYear <= 57 && lastFour <= 8999)
+            {
+                firstTwo = 20;
+            }
+            else if (lastFour <= 8999)
+            {
+                firstTwo = 18;
+            }
+            else if (lastTwoOfYear <= 36 && lastFour <= 9999)
+            {
+                firstTwo = 20;
+            }
+            else
+            {
+                firstTwo = 19;
+            }
+
+            return firstTwo * 100 + lastTwoOfYear;
+
+        }
+    }
+}
---
+++ ListMerger/MergeList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListMerger
+{
+    class MergeList
+    {
+        public static List<T> Merge<T>(List<T> list1, List<T> list2) where T : IComparable<T>
+        {
+            List<T> mergedList = new List<T>();
+
+            var list1Id = 0;
+            var list2Id = 0;
+
+
+            for (int i = 0; i < list1.Count + list2.Count; i++)
+            {
+                if(list1Id >= list1.Count)
+                {
+                    for (int j = list2Id; j < list2.Count; j++)
+                    {
+                        mergedList.Add(list2[j]);
+                    }
+                    return mergedList;
+                }else if (list2Id >= list2.Count)
+                {
+                    for (int j = list1Id; j < list1.Count; j++)
+                    {
+                        mergedList.Add(list1[j]);
+                    }
+                    return mergedList;
+                }
+
+                if(list1[list1Id].CompareTo(list2[list2Id]) <= 0)
+                {
+                    mergedList.Add(list1[list1Id]);
+                    list1Id++;
+                }
+                else
+                {
+                    mergedList.Add(list2[list2Id]);
+                    list2Id++;
+                }
+
+            }
+
+            return mergedList;
+        }
+    }
+}
